ebc00100762f4c94e1f63da3bc1b2266eebc6135
Albert Prince Mensah <prince.mensah@turntabl.io>
2021-11-04 15:31:04 +0000

    parsed and printed out descriptions of xml url

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,7 +4,11 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import org.jsoup.parser.Parser
+import javax.lang.model.util.Elements
+
+//import org.jsoup.select.Elements
 
 val rss_site = "http://rss.cnn.com/rss/edition_africa.rss"
 
@@ -14,7 +18,16 @@ fun main() {
 //    }.start(wait = true)
 //    println("hello")
     val doc = Jsoup.connect(rss_site).parser(Parser.xmlParser()).get()
-    println(doc)
+//    println(doc)
+//    log(doc.title())
+//    println(doc.select("a").first())
+    val link: Element = doc.select("description").first()
+    println(link)
+    for(some in doc.select("description")){
+        println(some)
+    }
+
+
 
 
 
